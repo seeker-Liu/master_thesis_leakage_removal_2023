Added masking and frequency characterist extract layer. Also fix for GPU weird bug

code/test.py
@@ -13,23 +13,28 @@ if __name__ == "__main__":
             no_gpu = True
     if no_gpu:
         tf.config.set_visible_devices([], 'GPU')
+    else:
+        physical_devices = tf.config.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
 
     data_files = [os.path.join(TEST_DIR, np_f) for np_f in os.listdir(TEST_DIR)]
 
     def avg_mse(y_true, y_pred):
         return (y_true - y_pred) * (y_true - y_pred) / tf.cast(tf.shape(y_true)[1], tf.float32)
     ckpt_folder = os.path.join(MODEL_DIR, "checkpoint")
-    last_model_path = os.listdir(ckpt_folder)[-1]
-    model = tf.keras.models.load_model(os.path.join(ckpt_folder, last_model_path),
-                                       custom_objects={"avg_mse": avg_mse})
+    last_model_name = sorted(os.listdir(ckpt_folder))[-1]
+    model_path = os.path.join(ckpt_folder, last_model_name)
+    print(f"Model path: {model_path}")
+    model = tf.keras.models.load_model(model_path, custom_objects={"avg_mse": avg_mse})
 
     for i, path in enumerate(data_files):
         print(f"{i}-th input, path: {path}")
 
         data = np.load(path)
 
-        input_mag = np.expand_dims(np.hstack((data["input_mag"], data["ref_mag"])), axis=0)
-        res_mag = np.squeeze(model(input_mag, training=False).numpy(), axis=0)
+        input_mag = np.expand_dims(data["input_mag"], axis=0)
+        ref_mag = np.expand_dims(data["ref_mag"], axis=0)
+        res_mag = np.squeeze(model((input_mag, ref_mag), training=False).numpy(), axis=0)
         print(f"MSE value of spectrogram: {np.sum((res_mag - data['truth_mag']) ** 2) / res_mag.shape[0]}")
 
         result_wav = istft_routine(res_mag, data["input_phase"]).copy()

code/tf_dataset.py
@@ -16,14 +16,15 @@ def get_dataset(t: str, use_spectrogram):
     if use_spectrogram:
         def dataset_mapper(filepath):
             data = np.load(filepath)
-            return np.hstack((data["input_mag"], data["ref_mag"])), data["truth_mag"]
+            return data["input_mag"], data["ref_mag"], data["truth_mag"]
     else:
         def dataset_mapper(filepath):
             data = np.load(filepath)
-            return np.vstack((data["input"], data["ref"])), data["truth"]
+            return data["input"], data["ref"], data["truth"]
 
     def dataset_mapper_wrapper(filepath):
-        return tf.numpy_function(dataset_mapper, [filepath], (tf.float32, tf.float32))
+        x1, x2, y = tf.numpy_function(dataset_mapper, [filepath], (tf.float32, tf.float32, tf.float32))
+        return (x1, x2), y
 
     dataset = dataset.map(dataset_mapper_wrapper).batch(1)
     return dataset

code/tf_model.py
@@ -1,12 +1,26 @@
 import tensorflow as tf
 
 
-def get_model():
+def old_get_model():
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.GRU(1000, return_sequences=True, input_shape=(None, 2049*2)))
-    model.add(tf.keras.layers.GRU(1000, return_sequences=True))
-    model.add(tf.keras.layers.Dense(2049))
+    model.add(tf.keras.layers.Dense(512, activation="relu", input_shape=(None, 2049*2)))
+    model.add(tf.keras.layers.GRU(256, return_sequences=True))
+    model.add(tf.keras.layers.GRU(256, return_sequences=True))
+    model.add(tf.keras.layers.Dense(2049, activation="sigmoid"))
 
+    return model
+
+
+def get_model():
+    mixed_spec = tf.keras.layers.Input(shape=(None, 2049), name="mixed_spec")
+    ref_spec = tf.keras.layers.Input(shape=(None, 2049), name="ref_spec")
+    concated = tf.keras.layers.Concatenate(axis=-1)([mixed_spec, ref_spec])
+    d = tf.keras.layers.Dense(512, activation="relu")(concated)
+    g1 = tf.keras.layers.GRU(256, return_sequences=True)(d)
+    g2 = tf.keras.layers.GRU(256, return_sequences=True)(g1)
+    mask = tf.keras.layers.Dense(2049, activation="sigmoid")(g2)
+    output = mask * mixed_spec
+    model = tf.keras.Model(inputs=[mixed_spec, ref_spec], outputs=output)
     return model
 
 